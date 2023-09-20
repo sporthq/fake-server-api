4e4fc45ce4d053ab1f21a235e23e33249b35f9b3
sporthq <nowak.sebastian991@gmail.com>
2023-09-20 14:00:55 +0200

    git init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const content = [{ content: 'Menu Jeden' }, { content: 'Menu Dwa' }, { content: 'Menu Trzy' }];
+	const [num, setNum] = useState(0);
+	return (
+		<div style={{ fontSize: '2rem' }} className=''>
+			<nav style={{ color: 'wheat' }}>
+				<ul
+					style={{
+						display: 'flex',
+						color: '#fff',
+						justifyContent: 'center',
+						gap: '8rem',
+						listStyle: 'none',
+						backgroundColor: '#333',
+						fontWeight: 'bold',
+						height: '100%',
+					}}
+				>
+					<Tab num={0} onClick={function(){
+						console.log('123')
+					}} content={'Menu Jeden'} />
+					<Tab num={1} content={'Menu Dwa'} onClick={() => setNum(1)} />
+					<Tab num={2} content={'Menu Trzu'} onClick={() => setNum(2)} />
+				</ul>
+			</nav>
+			<Content color={'#888'} content={content[num].content}></Content>
+		</div>
+	);
 }
 
+function Tab({ num, content, onClick={onClick} }) {
+	return (
+		<li onClick={()}  num={num} style={{ cursor: 'pointer' }}>
+			{content}
+		</li>
+	);
+}
+
+function Content({ color, children, content }) {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: color,
+				color: 'white',
+				height: '50vh',
+			}}
+		>
+			{content}
+		</div>
+	);
+}
 export default App;
